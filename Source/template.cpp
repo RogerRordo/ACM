fd85189966eb095d717250decc44eb8136623fa3
Z. F. LUO <luonagamitsu@MacBook-Pro.local>
2016-10-01 18:07:45 +0800

    My Template

---
+++ Source/template.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<vector>
+#include<queue>
+#include<set>
+#include<map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#define ll long long
+#define tr(i,l,r) for((i)=(l);(i)<=(r);++i)
+using namespace std;
+int read()
+{
+    int x=0,f=1;
+    char ch=getchar();
+    while (ch<'0'||ch>'9') {if (ch=='-') f=-1; ch=getchar();}
+    while (ch>='0'&&ch<='9') {x=x*10+ch-'0'; ch=getchar();}
+    return x*f;
+}
+int main()
+{
+
+    return 0;
+}
