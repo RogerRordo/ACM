e44baa56fe78cacbc548de6c35c65d6a8ad5c556
Z. F. LUO <luonagamitsu@MacBook-Pro.local>
2016-10-12 14:08:07 +0800

    ACM

--- Source/template.cpp
+++ Source/template.cpp
@@ -13,6 +13,7 @@
 #include<map>
 #include<cmath>
 #include<algorithm>
+#include<ctime>
 #include<bitset>
 #define ll long long
 #define tr(i,l,r) for((i)=(l);(i)<=(r);++i)
@@ -26,8 +27,16 @@ int read()
     while (ch>='0'&&ch<='9') {x=x*10+ch-'0'; ch=getchar();}
     return x*f;
 }
+void write(int x)
+{
+    char a[20],s=0;
+    if (x==0){putchar('0'); return ;}
+    while (x) {a[s++]=x%10+'0'; x=x/10;}
+    while (s--) putchar(a[s]);
+}
+void writeln(int x){write(x); putchar('\n');}
 int main()
 {
-
+    
     return 0;
 }
