382d5b0a103b05afce711a53215aa99bb25e50ad
Z. F. LUO <luonagamitsu@MacBook-Pro.local>
2016-10-14 12:25:00 +0800

    ACM

--- Source/template_geometry.cpp
+++ Source/template_geometry.cpp
@@ -19,6 +19,8 @@
 #define tr(i,l,r) for((i)=(l);(i)<=(r);++i)
 #define oo 0x7F7F7F7F
 #define eps 1E-8
+#define nonx 1E30
+#define maxps 10005
 using namespace std;
 int cmp(double x)
 {
@@ -44,6 +46,8 @@ struct point
     
     friend double len(point a){return sqrt(a.x*a.x+a.y*a.y);}
     friend point rotate(point a,double b){return point(a.x*cos(b)-a.y*sin(b),a.x*sin(b)+a.y*cos(b));}
+    friend double angle(point a,point b){return acos(a*b/len(a)/len(b));}
+    //friend point reflect(point a,point b){}
 } ;
 struct line
 {
@@ -52,26 +56,66 @@ struct line
     line(point pa,point pb){a=pa;b=pb;}
     point dir(){return b-a;}
     
-    friend point pl_proj(point a,line b)
+    friend point proj(point a,line b)
         {double t=(a-b.a)*b.dir()/(b.dir()*b.dir());return point(b.a+t*b.dir());}
-    friend double pl_dist(point a,line b){return len(pl_proj(a,b)-a);}
-    friend bool pl_onray(point a,line b)
+    friend double dist(point a,line b){return len(proj(a,b)-a);}
+    friend bool onray(point a,line b)
         {return cmp((a-b.a)^b.dir())==0&&cmp((a-b.a)*b.dir())>=0;}
-    friend bool pl_onseg(point a,line b){return pl_onray(a,b)&&pl_onray(a,line(b.b,b.a));}
-    friend bool pl_online(point a,line b){return pl_onray(a,b)||pl_onray(a,line(b.b,b.a));}
-    friend bool ll_parallel(line a,line b){return cmp(a.dir()^b.dir())==0;}
-    friend point ll_cross(line a,line b){}
-    friend ld_line move(line a,double b){}
-    
-} ;
-struct polygon
-{
-    
+    friend bool onseg(point a,line b){return onray(a,b)&&onray(a,line(b.b,b.a));}
+    friend bool online(point a,line b){return onray(a,b)||onray(a,line(b.b,b.a));}
+    friend bool parallel(line a,line b){return cmp(a.dir()^b.dir())==0;}
+    //friend point ll_crossline(line a,line b){}
+    //friend point ll_crossseg(line a,line b){}
+    //friend line ld_line move(line a,double b){}
+    //friend bool sameside(point a,point b,line c)
+    //friend point reflect(line a,point b){}
+    //friend line reflect(line a,line b){}
+    //friend line reflect(point a,line b){}
 } ;
 struct circle
 {
+    point o; double r;
+    circle(){}
+    circle(point a,double b){o=a;r=b;}
     
+    //friend double S(circle a){}
+    //friend double C(circle a){}
+    //friend line cross(line a,circle b){}
+    //friend line cross(circle a,circle b){}
+    //friend line tangent(point a,circle b){}
+    //friend double crossS(vector<circle>){}
 } ;
+struct triangle//t 因triangle亦属polygon，故省去许多函数
+{
+    point a,b,c;
+    triangle(){}
+    triangle(point ta,point tb,point tc){a=ta;b=tb;c=tc;}
+    
+    //friend double S(triangle a){}
+    //friend double C(triangle a){}
+    //friend circle incircle(triangle a){}
+    //friend circle outcircle(triangle a){}
+    //friend point mc(triangle a){}
+    //friend point oc(triangle a){}
+} ;
+struct polygon
+{
+    int ps;
+    point a[maxps];
+    //void sort(){}
+    
+    //friend double S(triangle a){}
+    //friend double C(triangle a){}
+    //friend bool in(point a,polygon b){}
+    //friend point mc(polygon a){}
+    //friend line diameter(polygon a){}
+    //friend polygon cross(polygon a,polygon b){}
+    //friend polygon cross(line a,polygon b){}
+    //friend double crossS(circle a,polygon b){}
+    //friend circle mincover(polygon a){}
+    //friend polygon core(polygon a){}
+} ;
+const point non=point(nonx,nonx);
 int read()
 {
     int x=0,f=1;
